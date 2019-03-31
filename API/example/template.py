97fa08ab334cfa45c47842440465486b031b11e8
Edward <eddyweddy@gmail.com>
2019-03-31 11:31:51 -0400

    travis test again

--- API/example/template.py
+++ API/example/template.py
@@ -1,4 +1,3 @@
-
 from flask_restful import Resource
 from flask import jsonify
 import git
--- tests/test_flaskApp.py
+++ tests/test_flaskApp.py
@@ -1,32 +1,5 @@
-# from flask import Flask
-# import pytest
-#
-# @pytest.fixture(scope='module')
-# def test_client():
-#     flask_app = create_app('flask_test.cfg')
-#
-#
-# def test_app_exists(self):
-#     self.assertFalse(current_app is None)
-#
-#
-#
-# def test_app_is_testing(self):
-#     self.assertTrue(current_app.config['TESTING'])
-
-# import flask_restful
-# from API.example.template import HelloWorld
-#
-#
-# def test_is_instance():
-#     instance = HelloWorld(flask_restful.Resource)
-#     assert isinstance(instance)
-
 import unittest
-from API.example.template import HelloWorld
 import requests
-import json
-import sys
 
 
 class TestFlaskApiUsingRequests(unittest.TestCase):
