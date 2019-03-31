fdab6ae1067aa019764029e522d30aa64303a95e
Edward <eddyweddy@gmail.com>
2019-03-31 11:52:01 -0400

    tag issues

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "${secure}" | docker login --username="${REGISTRY_USER}" --password-stdin
-docker push "${REGISTRY_USER}"/"${REPO}"
+docker push "${REGISTRY_USER}"/"${REPO}":latest
